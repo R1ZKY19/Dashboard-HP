2dee22bf1459e285c25a972f0a68cc5839e8260e
IKY <rizkykucuk19@gmail.com>
2026-08-19 05:25:07 +0700

    Fix dashboard config for authenticated session

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const CONFIG = {
   API_URL: "https://script.google.com/macros/s/AKfycbxL32gf2D7AOOms72gNwdHuN4N3BuSD_s1M-CYO-7WaQP8ZJ6TYA2tGuXqUAetHfX05SQ/exec",
-  APP_NAME: "OFFICE DATA CENTER"
+  APP_NAME: "OFFICE DATA CENTER",
+  SESSION_KEY: "office_dashboard_session"
 };
