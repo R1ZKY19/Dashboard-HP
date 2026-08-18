b96b31724a56bca5d9b2c786bac30cc3ea26fb7d
IKY <rizkykucuk19@gmail.com>
2026-08-19 06:50:22 +0700

    Point dashboard to latest Apps Script deployment

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbxO2AB9GbDyOQPORcmwzjmWTYcm2lR3sZWuX3oRZZyV-tzQ7CItzaZr2O2oFc4Tk_iypA/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbyNREOBPwm7Lg4FcoYjiQH4Y7KXMKqWP433Ut1hAmPg04Y7p1tQsKceeAbdrmYU6SJotQ/exec",
   APP_NAME: "OFFICE DATA CENTER",
   SESSION_KEY: "office_dashboard_session"
 };
