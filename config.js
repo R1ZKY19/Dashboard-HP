5efab2ffee57edfacc2ca1fb02832076947420a5
IKY <rizkykucuk19@gmail.com>
2026-08-19 06:22:28 +0700

    Update Apps Script Web App deployment URL

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbzzLBcSDSwFyHDt5zvZ15jaQ7HE0x8HaNNPWbpUuiFyqf954bXtfQwYUf1z_IBTiFYbSg/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbw0L0_nDbn9k3LK4LYuM0yHuiTodqCiBGP2Z8xgK6M2albuk-4hqtBnePW8c3S0a9Nrxg/exec",
   APP_NAME: "OFFICE DATA CENTER",
   SESSION_KEY: "office_dashboard_session"
 };
