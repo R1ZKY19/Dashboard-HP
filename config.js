83a9ead756ed4a6ec8f18f855c37a661e7bdfa7d
IKY <rizkykucuk19@gmail.com>
2026-08-20 02:59:13 +0700

    config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbxd9PPWShGjf4rNRTdoLgTnLu3sDAdg_skPiPiNI-YnGOQ2gui-7Vp2ui2u9rZkkAuBdw/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbwSEh5GEc22EOZUdkaiHznwibGg0nZVMXkJA0JlOdARRUjG2OzsGxfWewTeNhgGQ3oY3g/exec",
   APP_NAME: "OFFICE DATA CENTER",
   SESSION_KEY: "office_dashboard_session"
 };
